c2a85a871d6c5cd63351f062f4d4c7998b93a8a4
Luka Trikha <lukadtrikha@gmail.com>
2023-04-25 13:55:30 -0400

    Add additional flask logic

--- wsgi/app.py
+++ wsgi/app.py
@@ -20,6 +20,15 @@ def closeDb():
         db.close()
 
 
+def sqlQuery(query: str, kwargs:dict):
+    with getDb().cursor() as cur:
+        cur.execute(query, kwargs)
+        result = cur.fetchone()
+        getDb().commit()
+
+    return result
+
+
 @app.before_request
 def beforeReq():
     if 'loggedIn' not in session:
@@ -47,16 +56,20 @@ def login():
         password = request.form['passwd']
         print(f'User Entered: {user}\nPassword Entered: {password}')
 
-        with getDb().cursor() as cur:
-            query = 'SELECT password FROM Users WHERE user_id=%(usr)s;'
-            vars = {'usr' : user}
-            cur.execute(query, vars)
-            result = cur.fetchone()
-            getDb().commit()
+
+
+        # with getDb().cursor() as cur:
+        query = 'SELECT first_name, password FROM Users WHERE email=%(usr)s;'
+        vars = {'usr' : user}
+            # cur.execute(query, vars)
+            # result = cur.fetchone()
+        result = sqlQuery(query, vars)
+        print(result)
+            # getDb().commit()
         
         if result != None and password == result['password']:
             session['loggedIn'] = True
-            session['user'] = user
+            session['user'] = result[0] 
             session.modified = True
             return redirect(url_for('success', user=session['user']))
         else:
@@ -69,13 +82,37 @@ def success(user=None):
 
 @app.route('/logout/')
 def logout():
-    session.pop("user", None)
-    session['loggedIn'] = False
+    session.clear()
     session.modified = True
     print(session)
     return render_template('logout.html')
 
 
-@app.route('/event-registration/', methods=['POST', 'GET'])
-def registration():
+@app.route('/event-registration/<user>/', methods=['POST', 'GET'])
+def registration(user=None):
+
+    if request.method == 'POST':
+        event_name = request.form['eventname']
+        est_attd = request.form['estimatedattendance']
+        event_desc = request.form['eventdescription']
+        first_loc_pref = request.form['firstLocationPreference']
+        second_loc_pref= request.form['secondLocationPreference']
+        date = request.form['eventdate']
+        start_time = request.form['starttime']
+        end_time = request.form['endtime']
+        session_user = session['user']
+
+        print(f'Event Name: {event_name}')
+        print(f'Estimated Attendance: {est_attd}')
+        print(f'Event Description: {event_desc}')
+        print(f'Primary Location: {first_loc_pref}')
+        print(f'Secondary Location: {second_loc_pref}')
+        print(f'Date entered: {date}')
+        print(f'Start time entered: {start_time}')
+        print(f'End time entered: {end_time}')
+        print(f'The user in question: {session_user}')
+
     return render_template('eventReg.html')
+
+if __name__ != '__main__':
+    application=app
