7718896ce67f8f260d30619403cd4d121a68ae19
Luka Trikha <lukadtrikha@gmail.com>
2023-05-07 21:39:46 -0400

    Fix use login bug. If not in a club, registration form is removed

--- wsgi/app.py
+++ wsgi/app.py
@@ -62,7 +62,7 @@ def login():
         password = request.form['passwd']
         print(f'User Entered: {user}\nPassword Entered: {password}')
 
-        query = 'SELECT first_name, password, user_id FROM Users WHERE email=%(usr)s;'
+        query = 'SELECT first_name, password, user_id, user_type FROM Users WHERE email=%(usr)s;'
         vars = {'usr' : user}
         result = sqlQuery(query, vars)
         print(result)
@@ -71,6 +71,9 @@ def login():
             session['loggedIn'] = True
             session['user'] = result[0]
             session['usr_id'] = result[2]
+            session['user_type'] = result[3]
+            print(result[3])
+            print(type(result[3]))
             session.modified = True
             return redirect(url_for('success', user=session['user']))
         else:
@@ -80,33 +83,34 @@ def login():
 @app.route('/success/<user>/', methods=['GET', 'POST'])
 def success(user=None):
     events = []
-
     user_kwargs = {
         "usr_id":session['usr_id']
     }
     club_query = "SELECT club_name FROM Members m, Users s\
         WHERE %(usr_id)s = m.user_id;"
-        # WHERE %(usr_id)s = %(usr_id)s;"
     
     club_result = sqlQuery(club_query, user_kwargs)
-    session['club_name'] = club_result[0]
+    print(club_result)
+    if club_result != None:
+        session['club_name'] = club_result[0]
 
-    kwargs = {
-        "clubname" : session['club_name']
-    }
-    query = "SELECT event_name, event_date, primary_loc FROM\
-        Event WHERE club_name = %(clubname)s;"
-    result = multiSqlQuery(query, kwargs)
+        kwargs = {
+         "clubname" : session['club_name']
+        }
+        query = "SELECT event_name, event_date, primary_loc FROM\
+            Event WHERE club_name = %(clubname)s;"
+        result = multiSqlQuery(query, kwargs)
 
-    if not result:
-        result
-    else:
         for i in result:
-            print(i)
-            print(type(i))
             events.append(i)
 
-    return render_template('success.html', user=session['user'], events=events)
+    # if not result:
+    #     result
+    # else:
+    #     for i in result:
+    #         events.append(i)
+
+    return render_template('success.html', user=session['user'], user_type=session['user_type'], events=events)
 
 @app.route('/logout/')
 def logout():
@@ -118,15 +122,15 @@ def logout():
 @app.route('/event-registration/<user>/', methods=['POST', 'GET'])
 def registration(user=None):
 
-    user_kwargs = {
-        "usr_id":session['usr_id']
-    }
-    club_query = "SELECT club_name FROM Members m, Users s\
-        WHERE %(usr_id)s = m.user_id;"
-        # WHERE %(usr_id)s = %(usr_id)s;"
+    # user_kwargs = {
+    #     "usr_id":session['usr_id']
+    # }
+    # club_query = "SELECT club_name FROM Members m, Users s\
+    #     WHERE %(usr_id)s = m.user_id;"
+    #     # WHERE %(usr_id)s = %(usr_id)s;"
     
-    club_result = sqlQuery(club_query, user_kwargs)
-    session['club_name'] = club_result[0]
+    # club_result = sqlQuery(club_query, user_kwargs)
+    # session['club_name'] = club_result[0]
 
     if request.method == 'POST':
         kwargs= {
@@ -140,25 +144,6 @@ def registration(user=None):
         "endtime" : request.form['endtime'],
         "clubname" : session['club_name'],
         }
-
-        # eventname = request.form['eventname']
-        # estimatedattendance = request.form['estimatedattendance']
-        # eventdescription = request.form['eventdescription']
-        # firstLocationPreference = request.form['firstLocationPreference']
-        # secondLocationPreference = request.form['secondLocationPreference']
-        # eventdate = request.form['eventdate']
-        # starttime = request.form['starttime']
-        # endtime = request.form['endtime']
-        # user = session['user']
-        # print(f'Event Name: {eventname}')
-        # print(f'Estimated Attendance: {estimatedattendance}')
-        # print(f'Event Description: {eventdescription}')
-        # print(f'Primary Location: {firstLocationPreference}')
-        # print(f'Secondary Location: {secondLocationPreference}')
-        # print(f'Date entered: {eventdate}')
-        # print(f'Start time entered: {starttime}')
-        # print(f'End time entered: {endtime}')
-        # print(f'The user in question: {user}')
 
         query = "INSERT INTO Event (\
             event_name, club_name, event_time,\
